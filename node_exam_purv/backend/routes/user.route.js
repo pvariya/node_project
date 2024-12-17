65634de24bb34082735b54eb3c3f21008f65f6e0
variyapurv <variyapurv4211@gmail.com>
2024-12-17 19:03:14 +0530

    cart

--- node_exam_purv/backend/routes/user.route.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const { signUp, login } = require('../controller/user.controller');
-const userRouter = express.Router();
-
-
-userRouter.post('/signup', signUp)
-userRouter.post('/login', login)
-
-module.exports = userRouter
